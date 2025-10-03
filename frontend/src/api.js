e78ba2afee251e5c534727cb1afebb7167d1435a
Loi Tran <88891057+loi9@users.noreply.github.com>
2025-10-03 16:55:49 +0700

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 // frontend/src/services/api.js
 import axios from "axios";
-console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://ink-manager-backend.onrender.com/api",
-  });
+  baseURL: "https://ink-manager-backend.onrender.com/api",
+});
+
 export default api;
